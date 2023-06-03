cb43bfabcd2440f60a3325d694896a4c924078b2
David Balland <morveus@morveus.com>
2023-06-04 01:24:09 +0200

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -12,4 +12,7 @@ chsh -s /home/linuxbrew/.linuxbrew/bin/fish
 brew install starship
 
 # Deploy my SSH public key 
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/BnhfaNr5onYZ8ZfbRz7BTsnC+Zvcjqjj5/QaJJUMzFQE+Urb1TXGKuaAG/IYt/pWIld5MGJt0T53w+AP7McrF9ROp1vwLYMKS058fXfOm8bab1DShfOBDNtnF3n/ojLN7CVXGDXgblxbxR+2svhzztldfXlm5kaj4g/UheZHkvOuLNWpyMgHUDxaL/kQXUK2dOqf5sarpFR1qgLpnLAXnbow76kfqJNm0YCkHsnLdpLoRX5RtD8POpEa+5NM7D9rwYby4dEMhMh2V26zJdBUcbGsMoHUjzQBGhvKft0XpGOBQO/e+X/UUq+//aLmVOhIZK1lqybynK+S0gsU/RYL
+mkdir ~/.ssh
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/BnhfaNr5onYZ8ZfbRz7BTsnC+Zvcjqjj5/QaJJUMzFQE+Urb1TXGKuaAG/IYt/pWIld5MGJt0T53w+AP7McrF9ROp1vwLYMKS058fXfOm8bab1DShfOBDNtnF3n/ojLN7CVXGDXgblxbxR+2svhzztldfXlm5kaj4g/UheZHkvOuLNWpyMgHUDxaL/kQXUK2dOqf5sarpFR1qgLpnLAXnbow76kfqJNm0YCkHsnLdpLoRX5RtD8POpEa+5NM7D9rwYby4dEMhMh2V26zJdBUcbGsMoHUjzQBGhvKft0XpGOBQO/e+X/UUq+//aLmVOhIZK1lqybynK+S0gsU/RYL" >> ~/.ssh/authorized_keys
+
+#
