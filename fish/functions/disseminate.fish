7294e0093f3923959dc4032b050a38c4020167b4
Morveus <morveus@morveus.com>
2023-10-14 18:10:35 +0200

    Auto restart avec github update

--- fish/functions/disseminate.fish
+++ fish/functions/disseminate.fish
@@ -28,6 +28,7 @@ function disseminate
 	       end
 	    else	    
 	       git clone https://github.com/Morveus/disseminate "$fish_folder/$repo_name" | mlolcat
+	       echo "Please wait while fish reloads scripts..." | lolcat
 	       sleep 5
                disseminate update
 	       return
