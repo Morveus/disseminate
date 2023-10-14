86c88d1293c4c3ec451adcfc125013100e0017cf
Morveus <morveus@morveus.com>
2023-10-14 15:44:08 +0200

    Add GO.sh

--- go.sh
+++ go.sh
@@ -15,3 +15,5 @@ fi
 git clone https://github.com/Morveus/disseminate "$HOME/.config/fish/disseminate"
 cp "$HOME/.config/fish/disseminate/fish/functions/*.fish" ""$HOME/.config/fish/functions"
 cp "$HOME/.config/fish/disseminate/fish/config.fish" ""$HOME/.config/fish/"
+
+echo "NOW type 'fish' then 'disseminate update'"
