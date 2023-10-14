18c06cbc54272ee76b686f0f53b1a2aa543af12a
David Balland <morveus@morveus.com>
2023-10-14 20:13:12 +0200

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -331,8 +331,8 @@ set --export PATH $BUN_INSTALL/bin $PATH
 set --export PATH /opt/homebrew/bin $PATH
 set --export PATH /home/linuxbrew/.linuxbrew/bin/ $PATH
 
-alias rainbow-spark='seq 1 (tput cols) | sort -R | spark | lolcat'
+alias rainbow-spark 'seq 1 (tput cols) | sort -R | spark | lolcat'
 alias display-machinename 'if test -f "$HOME/.config/fish/machinename"; pyfiglet (cat "$HOME/.config/fish/machinename") | mlolcat; else; pyfiglet $hostname | mlolcat; end'
-alias fish_greeting='rainbow-spark;display_machinename;rainbow-spark'
+alias fish_greeting 'rainbow-spark;display_machinename;rainbow-spark'
 
 alias clear='echo -e "\033[2J\033[H";fish_greeting;'
