8df58c662d2ae3067964753bf28e50aa6369df1d
David Balland <morveus@morveus.com>
2023-10-14 19:08:14 +0200

    Create go-android.sh

---
+++ go-android.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/Morveus/disseminate $HOME/.config/fish/disseminate
+
+cp $HOME/.config/fish/disseminate/fish/config.fish HOME/.config/fish/
+cp $HOME/.config/fish/disseminate/fish/functions/*.fish HOME/.config/fish/functions/
