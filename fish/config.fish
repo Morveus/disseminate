0403de90804a139c9a07ea3a44c8ab7a7dcb012c
David Balland <morveus@morveus.com>
2023-10-14 20:21:15 +0200

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -335,9 +335,9 @@ alias rainbow-spark 'seq 1 (tput cols) | sort -R | spark | lolcat'
 
 function display-machinename
     if test -f "$HOME/.config/fish/machinename"
-        pyfiglet (cat "$HOME/.config/fish/machinename") | mlolcat
+        pyfiglet (cat "$HOME/.config/fish/machinename") | head -n -1 | mlolcat
     else
-        pyfiglet $hostname | mlolcat
+        pyfiglet $hostname | head -n -1 | mlolcat
     end
 end
 
