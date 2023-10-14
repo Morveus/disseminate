2443eeba8ed823c936994cca262411a6eb55671f
Morveus <morveus@morveus.com>
2023-10-14 15:42:10 +0200

    Add GO.sh

---
+++ go.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ "$(uname)" == "Linux" ]]; then
+    if ! whereis fish > /dev/null; then
+        sudo apt update && sudo apt install fish wget curl -y
+    fi
+elif [[ "$(uname)" == "Darwin" ]]; then
+    if ! whereis fish > /dev/null; then
+        /bin/bash -c "$(curl -fsSL 
+https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        brew install fish git wget curl
+    fi
+fi
+
+git clone https://github.com/Morveus/disseminate "$HOME/.config/fish/disseminate"
+cp "$HOME/.config/fish/disseminate/fish/functions/*.fish" ""$HOME/.config/fish/functions"
+cp "$HOME/.config/fish/disseminate/fish/config.fish" ""$HOME/.config/fish/"
