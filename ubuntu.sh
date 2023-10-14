24d96eab1f38638b4a8dca4650b38a6800afbbf5
David Balland <morveus@morveus.com>
2023-10-14 02:04:10 +0200

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -27,5 +27,5 @@ fisher install jorgebucaran/spark.fish
 
 # Fish config 
 echo "alias clear='echo -en \"\\x1b[2J\\x1b[1;1H\" ; echo; echo; seq 1 (tput cols) | sort -R | spark | lolcat; echo; echo'" >> ~/.config/fish/config.fish 
-echo "alias rainbow-spark='seq 1 (tput cols) | sort -R | spark | lolcat;'" >> ~/.config/fish/config.fish
-echo "alias rainbow-line='string repeat -n (tput cols) (echo -e \"\\u2588\") | lolcat;'" >> ~/.config/fish/config.fish
+echo "alias rainbow-spark='seq 1 (tput cols) | sort -R | spark | lolcat'" >> ~/.config/fish/config.fish
+echo "alias rainbow-line='string repeat -n (tput cols) (echo -e \"\\u2588\") | lolcat'" >> ~/.config/fish/config.fish
