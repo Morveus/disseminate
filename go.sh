d8db2f8a93d09d40a87041acf5bb210987fd7b71
Morveus <morveus@morveus.com>
2023-10-14 15:50:19 +0200

    Update.sh

--- go.sh
+++ go.sh
@@ -6,8 +6,7 @@ if [[ "$(uname)" == "Linux" ]]; then
     fi
 elif [[ "$(uname)" == "Darwin" ]]; then
     if ! whereis fish > /dev/null; then
-        /bin/bash -c "$(curl -fsSL 
-https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
         brew install fish git wget curl
     fi
 fi
