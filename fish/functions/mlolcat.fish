9bf65d0618242c4048395adf9993ddb160fa61d0
Morveus <morveus@morveus.com>
2023-10-14 18:30:34 +0200

    Android-specific update

--- fish/functions/mlolcat.fish
+++ fish/functions/mlolcat.fish
@@ -1,13 +1,9 @@
 function mlolcat
-    read -d \n -z input
+   read -d \n -z input
 
-    if is_unraid
-        echo -n $input | lolcat # yeah I know
-    else
-       if is_debian; or is_mac
-           echo -n $input | lolcat
-       else
-           echo -n $input
-       end
-    end
+   if is_debian; or is_mac; or is_android; or is_unraid
+      echo -n $input | lolcat
+   else
+      echo -n $input
+   end
 end
