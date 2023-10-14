61a3e180305e86a0901c40c09ade32a349c3c79c
Morveus <morveus@morveus.com>
2023-10-14 18:47:33 +0200

    Added keys update for unraid

--- fish/functions/disseminate_unraid.fish
+++ fish/functions/disseminate_unraid.fish
@@ -1,7 +1,18 @@
 function disseminate_unraid
+   set keysfile $HOME/.ssh/authorized_keys
+   set keysfileboot /boot/config/authorized_keys
+   set gofile /boot/config/go
+
    if not type -q lolcat
 	pip3 install lolcat
    end
 
-   
+   if test -e $keysfile
+	cp $keysfile $keysfileboot
+   end
+
+   if ! grep -q "$keysfileboot" $gofile; then
+     echo "cp -p $keysfileboot $keysfile" >> /boot/config/go
+   fi
+      
 end
