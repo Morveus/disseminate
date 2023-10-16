54e657ef5cf21cf9f3b7a800c22b6905353520ff
Morveus <morveus@morveus.com>
2023-10-16 16:23:49 +0200

    ADD functions

--- fish/functions/_morveus-kubernetes-full-rollout.fish
+++ fish/functions/_morveus-kubernetes-full-rollout.fish
@@ -1,4 +1,4 @@
-function kubernetes-full-rollout
+function _morveus-kubernetes-full-rollout
     kubectl get deployments --all-namespaces -o json | jq -c -r '.items[] | "\(.metadata.namespace) \(.metadata.name)"' | while read -l ns deployment
         kubectl rollout restart deployment $deployment -n $ns
     end
--- fish/functions/morveus.fish
+++ fish/functions/morveus.fish
@@ -1,4 +1,4 @@
-function morveus
+$function morveus
     echo "Morveus Helper 1.0 ~~ https://morve.us/dis" | mlolcat
 
     switch $argv[1]
@@ -7,14 +7,25 @@ function morveus
         case connect
             switch $argv[2]
                 case home
+                    echo "Connecting to rack1" | mlolcat
                     ssh -t morveus@morve.us ssh morveus@rack1.morve.us
                 case vps
+                    echo "Connecting to morve.us" | mlolcat
                     ssh morveus@morve.us
                 case nas
+                    echo "Connecting to unRAID" | mlolcat
                     ssh -t morveus@morve.us ssh -t morveus@rack1.morve.us ssh root@192.168.1.10
+                case streamer
+                    echo "Connecting to the Volumio streamer" | mlolcat
+                    sshpass -p 'volumio' ssh volumio@streamer.morve.us
                 case '*'
                     echo "Invalid connection. Try 'home', 'vps' or 'nas'."
             end
+        case kubernetes
+            switch $argv[2]
+                case rollout-everything
+                    echo "Rolling out all deployments on the local k8s cluster" | mlolcat
+                    _morveus-kubernetes-full-rollout                  
         case help
             echo "Usage:"
             echo "morveus connect [home|vps|nas] : quick connect to the main entrypoints at home"
