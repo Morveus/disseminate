0287d41a5bca3da5b1140fa1804bd349b39a7b22
David Balland <morveus@morveus.com>
2023-10-16 13:22:44 +0200

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -348,10 +348,3 @@ end
 
 alias fish-greeting 'rainbow-spark;display-machinename;rainbow-spark'
 alias fish_greeting 'fish-greeting'
-
-function kubernetes-full-rollout
-    kubectl get deployments --all-namespaces -o json | jq -c -r '.items[] | "\(.metadata.namespace) \(.metadata.name)"' | while read -l ns deployment
-        kubectl rollout restart deployment $deployment -n $ns
-    end
-end
-funcsave kubernetes-full-rollout
