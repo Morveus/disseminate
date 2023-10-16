024efe827432c75d36e3df7e8f348420ae74e6c9
David Balland <morveus@morveus.com>
2023-10-16 13:23:16 +0200

    Create kubernetes-full-rollout.fish

---
+++ fish/functions/kubernetes-full-rollout.fish
@@ -0,0 +1,5 @@
+function kubernetes-full-rollout
+    kubectl get deployments --all-namespaces -o json | jq -c -r '.items[] | "\(.metadata.namespace) \(.metadata.name)"' | while read -l ns deployment
+        kubectl rollout restart deployment $deployment -n $ns
+    end
+end
