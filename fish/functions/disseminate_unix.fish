57d11f2788bd46dc7e13520665e2be127cc5a048
David Balland <morveus@morveus.com>
2023-10-18 15:29:53 +0200

    Update disseminate_unix.fish

--- fish/functions/disseminate_unix.fish
+++ fish/functions/disseminate_unix.fish
@@ -1,6 +1,6 @@
 function disseminate_unix
   if is_raspberry
-    echo "Running on Raspberry Pi, skipping Homebrew installation."
+    echo "Running on Raspberry Pi, skipping Homebrew installation." | mlolcat
   else if not type -q brew 
     /bin/bash -c "(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   end
