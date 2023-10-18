0739b6100bf6f36b510f0f3be1f964599c605c8c
David Balland <morveus@morveus.com>
2023-10-18 14:34:34 +0200

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [[ "$(uname)" == "Linux" ]]; then
-    sudo apt update && sudo apt install fish wget git python3 python3-pip python3-pyfiglet lolcat -y
+    sudo apt update && sudo apt install fish wget git python3 python3-pip python3-pyfiglet lolcat sshpass -y
     chsh -s /usr/bin/fish
 elif [[ "$(uname)" == "Darwin" ]]; then
     if ! whereis fish > /dev/null; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        brew install fish git wget curl
+        brew install fish git wget curl lolcat sshpass
 	echo "/home/linuxbrew/.linuxbrew/bin/fish" | sudo tee -a /etc/shells
 	chsh -s /home/linuxbrew/.linuxbrew/bin/fish
     fi
