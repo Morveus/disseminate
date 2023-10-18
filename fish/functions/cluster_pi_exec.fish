b73cce3a4f3f6ac7e03c72f05961ae154e9f2aef
David Balland <morveus@morveus.com>
2023-10-18 20:16:36 +0200

    Update cluster_pi_exec.fish

--- fish/functions/cluster_pi_exec.fish
+++ fish/functions/cluster_pi_exec.fish
@@ -1,4 +1,5 @@
-function cluster_pi_exec --argument ssh_command
+function cluster_pi_exec
+  set ssh_command $argv[1]
   for i in (seq 101 114)
       ssh -o StrictHostKeyChecking=no morveus@10.10.10.$i $ssh_command
   end
