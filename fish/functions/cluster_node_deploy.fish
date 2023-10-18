893aec9dcedc409bd35133f9f29e9a9f8e8031e1
David Balland <morveus@morveus.com>
2023-10-18 16:59:18 +0200

    Update cluster_node_deploy.fish

--- fish/functions/cluster_node_deploy.fish
+++ fish/functions/cluster_node_deploy.fish
@@ -1,6 +1,16 @@
 function cluster_node_deploy
    echo "" | sudo tee /etc/motd > /dev/null
 
+   if type -q k3s
+      echo "k3s is installed." | mlolcat
+   else
+      echo "k3s is not installed."
+      echo -e "\033[31mk3s is not installed\033[0m"
+      echo -e "\033[31mPlease run /usr/local/bin/k3s-agent-uninstall.sh to uninstall it from an agent\033[0m"
+      echo -e "\033[31mor /usr/local/bin/k3s-uninstall.sh to uninstall it from a master.\033[0m"
+      return 1
+   end
+
    set main_ip (ip route get 1 | awk '{print $(NF-2);exit}')
    set last_byte (echo $main_ip | cut -d '.' -f 4)
 
@@ -46,6 +56,35 @@ function cluster_node_deploy
 
    sudo apt install -y open-iscsi lsscsi sg3-utils multipath-tools scsitools cifs-utils nfs-common
 
+
+   echo "Creating mountpoints..." | mlolcat
+   sudo mkdir -p /mnt/user/MegaNAS
+   sudo mkdir -p /mnt/iscsi_k8s
+
+   echo "Connecting to the various NAS servers..." | mlolcat
+   set fstab_line "192.168.1.10:/mnt/user/MegaNAS /mnt/user/MegaNAS nfs rw,soft,timeo=100 0 0"
+   
+   if not grep -q "MegaNAS" /etc/fstab
+       echo $fstab_line | sudo tee -a /etc/fstab > /dev/null
+       echo "Added MegaNAS to /etc/fstab" | mlolcat
+   else
+       echo "MegaNAS already in /etc/fstab" | mlolcat
+   end
+
+   set fstab_line "//192.168.1.20/k8s /mnt/iscsi_k8s cifs username=cluster,password=cluster123456!,uid=$username,gid=$username,iocharset=utf8 0 0"
+   
+   if not grep -q "iscsi_k8s" /etc/fstab
+       echo $fstab_line | sudo tee -a /etc/fstab > /dev/null
+       echo "Added FlashNAS to /etc/fstab" | mlolcat
+   else
+       echo "FlashNAS already in /etc/fstab" | mlolcat
+   end
+
+   echo "systemctl daemon-reload..." | mlolcat
+   sudo systemctl daemon-reload
+   echo "Mounting..." | mlolcat
+   sudo mount -a
+
    echo "Adding node to the cluster..." | mlolcat
    echo "Please provide the K8S master token" | mlolcat
    echo "(sudo cat /var/lib/rancher/k3s/server/node-token on the server)"
@@ -57,42 +96,8 @@ function cluster_node_deploy
       return 1
    end
 
-   echo "Please provide the FlashNAS smb password" | mlolcat
-   read -l input
-   if test -n "$input"
-      set smbpassword $input
-   else
-      echo -e "\033[31mPASSWORD REQUIRED\033[0m"
-      return 1
-   end
-
    echo "Token: $master_token" | mlolcat; echo;
-   echo "Connecting to the various NAS servers..." | mlolcat
 
-   set fstab_line "192.168.1.10:/mnt/user/MegaNAS /mnt/user/MegaNAS nfs rw,soft,timeo=100 0 0"
    
-   if not grep -q "MegaNAS" /etc/fstab
-       echo $fstab_line | sudo tee -a /etc/fstab > /dev/null
-       echo "Added MegaNAS to /etc/fstab" | mlolcat
-   else
-       echo "MegaNAS already in /etc/fstab" | mlolcat
-   end
-
-   set fstab_line "//192.168.1.20/k8s /mnt/iscsi_k8s cifs username=admin,password=$smbpassword,uid=$username,gid=$username,iocharset=utf8 0 0"
-   
-   if not grep -q "iscsi_k8s" /etc/fstab
-       echo $fstab_line | sudo tee -a /etc/fstab > /dev/null
-       echo "Added FlashNAS to /etc/fstab" | mlolcat
-   else
-       echo "FlashNAS already in /etc/fstab" | mlolcat
-   end
-
-   echo "Creating mountpoints..." | mlolcat
-   sudo mkdir -p /mnt/user/MegaNAS
-   sudo mkdir -p /mnt/iscsi_k8s
-
-   echo "systemctl daemon-reload..." | mlolcat
-   sudo systemctl daemon-reload
-   echo "Mounting..." | mlolcat
-   sudo mount -a
 end
+
