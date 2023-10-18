441b3051e1a169430e30e591f00c350c38088d39
David Balland <morveus@morveus.com>
2023-10-18 16:11:19 +0200

    Update cluster_node_deploy.fish

--- fish/functions/cluster_node_deploy.fish
+++ fish/functions/cluster_node_deploy.fish
@@ -1,5 +1,5 @@
 function cluster_node_deploy
-   > /etc/motd
+   echo '' > /etc/motd
    set main_ip (ip route get 1 | awk '{print $(NF-2);exit}')
    set last_byte (echo $main_ip | cut -d '.' -f 4)
 
