93d85901a9bd8699d91381177bd915c288a75c8c
David Balland <morveus@morveus.com>
2023-10-18 15:38:26 +0200

    Update pi_node_deploy.fish

--- fish/functions/pi_node_deploy.fish
+++ fish/functions/pi_node_deploy.fish
@@ -1,16 +1,17 @@
 function pi_node_deploy
-   echo "Enter node number:"
+   echo "ADDING A NEW NODE TO THE CLUSTER" | mlolcat
+   echo "Please enter the node number:" | mlolcat
    read node_number
    set node_name "pi-$node_number"
    set fqdn "pi$node_number.cluster.morve.us"
    
-   echo "Node name (default: $node_name):"
+   echo "What will be the node name? (default: $node_name)" | mlolcat
    read -l input
    if test -n "$input"
        set node_name $input
    end
    
-   echo "FQDN (default: $fqdn):"
+   echo "And its DNS name? (default: $fqdn)" | mlolcat
    read -l input
    if test -n "$input"
        set fqdn $input
