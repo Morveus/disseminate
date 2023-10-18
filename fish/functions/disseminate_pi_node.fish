81951f77b269d6692c92d704b88b453f26cb4dae
David Balland <morveus@morveus.com>
2023-10-18 13:40:05 +0200

    Update disseminate_pi_node.fish

--- fish/functions/disseminate_pi_node.fish
+++ fish/functions/disseminate_pi_node.fish
@@ -1,3 +1,16 @@
 function disseminate_pi_node
    
 end
+
+function is_raspberry
+   if test -f /proc/device-tree/model
+       set model (cat /proc/device-tree/model)
+       if string match -r 'Raspberry Pi' $model
+           return 0
+       else
+           return 1
+       end
+   else
+       return 1
+   end
+end
