0089b2a24b7ce2f8be9610a5ec7efb6ea57f168f
David Balland <morveus@morveus.com>
2023-10-18 15:25:33 +0200

    Update disseminate_pi_node.fish

--- fish/functions/disseminate_pi_node.fish
+++ fish/functions/disseminate_pi_node.fish
@@ -1,5 +1,28 @@
 function disseminate_pi_node
+
+end
+
+function pi_node_deploy
+   echo "Enter node number:"
+   read node_number
+   set node_name "pi-$node_number"
+   set fqdn "pi$node_number.cluster.morve.us"
    
+   echo "Node name (default: $node_name):"
+   read -l input
+   if test -n "$input"
+       set node_name $input
+   end
+   
+   echo "FQDN (default: $fqdn):"
+   read -l input
+   if test -n "$input"
+       set fqdn $input
+   end
+   
+   echo "Node number: $node_number"
+   echo "Node name: $node_name"
+   echo "FQDN: $fqdn"
 end
 
 function is_raspberry
