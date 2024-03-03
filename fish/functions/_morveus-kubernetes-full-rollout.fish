cf0e7cbc68d780ce91effb9aed94ba3da726c8c0
Morveus <morveus@morveus.com>
2024-03-03 12:53:50 +0100

    Add function scaling deployments using NAS

--- fish/functions/_morveus-kubernetes-full-rollout.fish
+++ fish/functions/_morveus-kubernetes-full-rollout.fish
@@ -3,3 +3,20 @@ function _morveus-kubernetes-full-rollout
         kubectl rollout restart deployment $deployment -n $ns
     end
 end
+
+function morveus_scale_NASbound_deployments
+    if test -z "$argv[1]"
+        echo "Error: Argument 'scale' (a number) is missing."
+        return 1
+    end
+
+    set REP $argv[1]
+
+    echo "Scaling replicas to $REP for deployments accessing unRAID NAS"
+
+    for DEP in (kubectl get deployments --all-namespaces -o yaml | grep -E "NAS_CIFS|MegaNAS" | grep "Deployment" | jq -r ".metadata.name")
+        set NS (kubectl get deployments --all-namespaces | grep "$DEP" | awk '{print $1}' | uniq)
+        echo "$DEP in $NS..."
+        kubectl scale deployment $DEP --replicas="$REP" -n $NS
+    end
+end
