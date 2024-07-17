0eaf2e99ff535434824b45d33bb4556153eb23e6
David Balland <morveus@morveus.com>
2024-07-17 08:00:45 +0200

    Update go-android.sh

--- go-android.sh
+++ go-android.sh
@@ -1,6 +1,17 @@
 apt update
+pkg up
+pkg up
+pkg in clang pkg-config libxml2 libxslt libjpeg-turbo -y
 apt install python -y
 pip install lolcat pyfiglet
+pip install imagemagick wget -y
+pkg install cronie termux-services -y
+
+#pip install cython lxml==5.0.0
+#pip install wanda -U 
+
+echo "Restart Termux and type 'sv-enable crond' 
+sleep 3
 
 git clone https://github.com/Morveus/disseminate $HOME/.config/fish/disseminate
 
